2881f7e679bac73fb6aeae00cbfe14625d575137
ADAM HARROWER <adamharrower16@gmail.com>
2023-06-04 18:56:25 +0000

    Display addistion questyion tot he user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,37 @@
 document.addEventListeners("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 
-    for(let button of buttons) {
+    for (let button of buttons) {
         button.addEventListeners("click", function() {
-            if (this.getAttribute("data-type") ==="submit") {
+          if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            } else{
+            } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame(){
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
 
+    // Creates two random numbers between 1 and 25 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+        
 }
 
 function checkAnswer(){
@@ -36,7 +53,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 
 }
 
